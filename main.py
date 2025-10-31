83145bd62ddf64a1a88b5b7be70e6eabf3059fc7
TahirSiddique092 <mohammedsiddique729@gmail.com>
2025-10-31 09:16:14 +0530

    Fixed wrong line no issue

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ for tok in lexer:
     print(f"{tok.type:<12} -> {tok.value}")
 
 print("\n--- PARSING --")
+lexer.input(data)
+lexer.lineno = 1 
 yacc.parser.parse(data, lexer=lexer)
 
 if not yacc.parse_errors:
